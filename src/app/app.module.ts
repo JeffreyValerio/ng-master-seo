c673616bef299b147d1e90c7ae26e1c93692c378
Jeffrey <jeffreyvalerio@hotmail.com>
2021-04-24 16:35:03 -0600

    others pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './frontend/shared/footer/footer.component';
+import { NavbarComponent } from './frontend/shared/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
